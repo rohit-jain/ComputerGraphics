22409f25a2fd741d7912134c178d65a0d17352e7
Rohit Jain <rohitjain.nsit@gmail.com>
2015-02-11 16:01:30 -0500

    trash

--- hw1/assignment.build/assignment.build/Objects-normal/x86_64/canvas.d
+++ hw1/assignment.build/assignment.build/Objects-normal/x86_64/canvas.d
@@ -1,9 +1,8 @@
-dependencies: \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/canvas.cpp \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/canvas.h \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/opengl.h \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/standard.h \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/geometry.h \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/vector.h \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/math.h \
-  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/matrix.h
+dependencies: /Users/rohitjain/github/ComputerGraphics/hw1/src/canvas.cpp \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/canvas.h \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/opengl.h \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/standard.h \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/geometry.h \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/vector.h \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/math.h \
+  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/matrix.h
