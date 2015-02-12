b4815eb76904f97abf9d1ac2e5439675124647e5
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-02-12 11:08:55 -0500

    Tetsing

--- hw1/assignment.build/assignment.build/Objects-normal/x86_64/main.d
+++ hw1/assignment.build/assignment.build/Objects-normal/x86_64/main.d
@@ -1,14 +1,15 @@
-dependencies: /Users/rohitjain/github/ComputerGraphics/hw1/src/main.cpp \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/opengl.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/standard.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/canvas.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/geometry.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/vector.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/math.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/core/matrix.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/scene.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/camera.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/model.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/object.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/primitive.h \
-  /Users/rohitjain/github/ComputerGraphics/hw1/src/tinyfiledialogs.h
+dependencies: \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/main.cpp \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/opengl.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/standard.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/canvas.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/geometry.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/vector.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/math.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/core/matrix.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/scene.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/camera.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/model.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/object.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/primitive.h \
+  /Users/oliverhoffman/Box\ Sync/Cornell\ Tech/Spring\ 2015/Computer\ Graphics/Assignments/Assignment\ 1/skeleton/hw1/src/tinyfiledialogs.h
